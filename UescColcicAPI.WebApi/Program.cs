dbe90ad85aa1602e755d5e7fa1251e899b266518
mslisboa <mathewslysboa@gmail.com>
2024-10-09 01:56:19 -0300

    feat: Conexão com o banco de dados

--- UescColcicAPI.WebApi/Program.cs
+++ UescColcicAPI.WebApi/Program.cs
@@ -1,9 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
